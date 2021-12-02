7c0a8ff0490b24a3d0ff30853941a1ca1c152756
Samo Prelog <samo@prelog.io>
2021-12-02 11:17:59 +0000

    day 2 - part 2

--- Day02.cs
+++ Day02.cs
@@ -2,27 +2,45 @@ namespace aoc_2021;
 
 public class Day02
 {
-    public static Vector2 ParseLine(string line)
-    {
-        var split = line.Split(Space);
-        var units = int.Parse(split[1]);
-        return split[0] switch
-        {
-            "forward" => Vector2.UnitX * units,
-            "up" => Vector2.UnitY * -1 * units,
-            "down" => Vector2.UnitY * units
-        };
-    }
+    public record struct SubmarineCommand(string command, int units);
+    public static Vector2 Dive_Part01(IEnumerable<SubmarineCommand> commands) =>
+        commands
+            .Aggregate(
+                Vector2.Zero,
+                (sum, line) => line.command switch
+                {
+                    "forward" => sum + Vector2.UnitX * line.units,
+                    "up" => sum + Vector2.UnitY * -1 * line.units,
+                    "down" => sum + Vector2.UnitY * line.units,
+                    _ => throw new InvalidOperationException($"Unexpected command {line.command}"),
+                });
 
-    public static int Dive_Part01(IEnumerable<string> commands) =>
-        commands.Select(ParseLine).Aggregate(Vector2.Zero, (sum, cur) => sum + cur, pos => (int)Math.Round(pos.X * pos.Y));
+    public static Vector2 Dive_Part02(IEnumerable<SubmarineCommand> commands) =>
+        commands
+            .Aggregate(
+                (position: Vector2.Zero, aim: 0),
+                (state, line) => line.command switch
+                {
+                    "forward" => (state.position + Vector2.UnitX * line.units + Vector2.UnitY * state.aim * line.units, state.aim),
+                    "up" => (state.position, state.aim - line.units),
+                    "down" => (state.position, state.aim + line.units),
+                    _ => throw new InvalidOperationException($"Unexpected command {line.command}"),
+                },
+                state => state.position);
 
     [Theory]
-    [InlineData("Day02_Example01", 150)]
-    [InlineData("Day02_Input", 2322630)]
-    public void Day02_Part01(string resource, int expectedResult)
+    [InlineData(nameof(Dive_Part01), "Day02_Example01", 150)]
+    [InlineData(nameof(Dive_Part01), "Day02_Input", 2322630)]
+    [InlineData(nameof(Dive_Part02), "Day02_Example01", 900)]
+    [InlineData(nameof(Dive_Part02), "Day02_Input", 2105273490)]
+    public void Tests(string method, string resource, int expectedResult)
     {
-        var data = LoadData(resource).Split(Environment.NewLine);
-        Assert.Equal(expectedResult, Dive_Part01(data));
+        var testMethod = typeof(Day02).GetMethod(method, BindingFlags.Public | BindingFlags.Static) ?? throw new ArgumentException($"Method {method} not found", nameof(method));
+        var data = LoadData(resource)
+            .Split(Environment.NewLine)
+            .Select(line => line.Split(Space))
+            .Select(lineSplit => new SubmarineCommand(command: lineSplit[0], units: int.Parse(lineSplit[1])));
+        var result = (Vector2) (testMethod.Invoke(null, new object[] {data}) ?? throw new InvalidOperationException($"Method {method} returned invalid data"));
+        Assert.Equal(expectedResult, (int)result.X * (int)result.Y);
     }
 }
--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,5 +2,6 @@ global using System.Collections.Generic;
 global using System.Collections.Immutable;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Numerics;
+global using System.Reflection;
 global using static aoc_2021.Utils;
 global using Xunit;
