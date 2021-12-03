90bd03ebc8122c32d96b64db7ceeaf9053792755
Samo Prelog <samo@prelog.io>
2021-12-03 08:42:35 +0000

    day 3 - part 2

--- Day03.cs
+++ Day03.cs
@@ -2,6 +2,8 @@ namespace aoc_2021;
 
 public class Day03
 {
+    private static int BinToDec(IEnumerable<bool> number) =>
+        number.Reverse().Aggregate((val: 0, exp: 1), (s, cur) => (val: cur ? s.val + s.exp : s.val, exp: s.exp * 2), s => s.val);
 
     [Theory]
     [InlineData("Day03_Example01", 198)]
@@ -10,21 +12,53 @@ public class Day03
     {
         var data = LoadData(resource)
             .Split(Environment.NewLine)
-            .Select(x => x.ToCharArray().Select(c => c == '0').ToImmutableArray())
+            .Select(x => x.ToCharArray().Select(c => c == '1').ToImmutableArray())
             .ToImmutableArray();
-        var gamma = 0;
-        var epsilon = 0;
         var len = data[0].Length;
+        var epsilon = new bool[len];
+        var gamma = new bool[len];
 
-        for (int i = 0, exp = 1; i < len; i++, exp *= 2)
+        for (int i = 0; i < len; i++)
         {
-            var bitPos = len - i - 1;
-            var ones = data.Select(x => x[bitPos]).Count(b => b);
+            var ones = data.Select(x => x[i]).Count(b => b);
             var zeroes = data.Length - ones;
-            epsilon += ones > zeroes ? exp : 0;
-            gamma += zeroes > ones ? exp : 0;
+            epsilon[i] = ones > zeroes;
+            gamma[i] = zeroes > ones;
         }
 
-        Assert.Equal(gamma * epsilon, expectedResult);
+        Assert.Equal(BinToDec(gamma) * BinToDec(epsilon), expectedResult);
+    }
+
+    [Theory]
+    [InlineData("Day03_Example01", 230)]
+    [InlineData("Day03_Input", 903810)]
+    public void Day03_Part02(string resource, int expectedResult)
+    {
+        var data = LoadData(resource)
+            .Split(Environment.NewLine)
+            .Select(x => x.ToCharArray().Select(c => c == '1').ToImmutableArray())
+            .ToImmutableList();
+
+        static ImmutableList<ImmutableArray<bool>> ReduceSet(ImmutableList<ImmutableArray<bool>> remaining, int position, Func<(int ones, int zeroes), bool> nextSetPredicate)
+        {
+            var ones = remaining.Where(x => x[position] == true).Count();
+            var zeroes = remaining.Count - ones;
+            var condition = nextSetPredicate((ones, zeroes));
+            return remaining.Where(x => x[position] == condition).ToImmutableList();
+        }
+
+        var len = data[0].Length;
+        var oxySet = data.ToImmutableList();
+        var co2Set = data.ToImmutableList();
+
+        for (var i = 0; i < len; i++)
+        {
+            oxySet = oxySet.Count > 1 ? ReduceSet(oxySet, i, x => x.ones >= x.zeroes) : oxySet;
+            co2Set = co2Set.Count > 1 ? ReduceSet(co2Set, i, x => x.ones < x.zeroes) : co2Set;
+        }
+
+        Assert.Single(oxySet);
+        Assert.Single(co2Set);
+        Assert.Equal(expectedResult, BinToDec(oxySet.Single()) * BinToDec(co2Set.Single()));
     }
 }
