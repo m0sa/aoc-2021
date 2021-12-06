46dbfa68f45132414588e304b462ac01971109e7
Samo Prelog <samo@prelog.io>
2021-12-06 08:43:25 +0000

    day 6 - part 2

--- Day06.cs
+++ Day06.cs
@@ -1,19 +1,9 @@
-using Xunit.Abstractions;
+using System.Buffers;
 
 namespace aoc_2021;
 
 public class Day06
 {
-
-    public ImmutableList<int> Day(IEnumerable<int> generation, int halflife = 7)
-    {
-        return generation
-            .Select(x => { var next = x - 1;  return next < 0 ? halflife - 1 : next; })
-            .Concat(Enumerable.Repeat(halflife + 1, generation.Where(x => x == 0).Count()))
-            .ToImmutableList();
-    }
-    private readonly ITestOutputHelper _output;
-    public Day06(ITestOutputHelper output) => _output = output;
     [Theory]
     [InlineData("Day06_Example01", 1, 5)]
     [InlineData("Day06_Example01", 2, 6)]
@@ -22,13 +12,32 @@ public class Day06
     [InlineData("Day06_Example01", 18, 26)]
     [InlineData("Day06_Example01", 80, 5934)]
     [InlineData("Day06_Input", 80, 343441)]
-    public void Part1(string intput, int days, int expectedCount)
+    [InlineData("Day06_Example01", 256, 26984457539)]
+    [InlineData("Day06_Input", 256, 1569108373832)]
+    public void Part1(string intput, int days, ulong expectedCount)
     {
-        var generation = LoadData(intput).Split(',').Select(int.Parse).ToImmutableList();
-        for (var i = 0; i < days; i++)
+        var generation = LoadData(intput).Split(',')
+            .Select(ulong.Parse)
+            .Aggregate(new ulong[9], (state, x) => {
+                state[x] = state[x] + 1u;
+                return state;
+            }, x => x);
+
+        for (var d = 0; d < days; d++)
         {
-            generation = Day(generation);
+            var newGeneration = ArrayPool<ulong>.Shared.Rent(9);
+            newGeneration[8] = generation[0];
+            newGeneration[7] = generation[8];
+            newGeneration[6] = generation[7] + generation[0];
+            newGeneration[5] = generation[6];
+            newGeneration[4] = generation[5];
+            newGeneration[3] = generation[4];
+            newGeneration[2] = generation[3];
+            newGeneration[1] = generation[2];
+            newGeneration[0] = generation[1];
+            Array.Copy(newGeneration, generation, 9);
+            ArrayPool<ulong>.Shared.Return(newGeneration);
         }
-        Assert.Equal(expectedCount, generation.Count);
+        Assert.Equal(expectedCount, generation.Aggregate(0ul, (sum, c) => sum + c));
     }
 }
