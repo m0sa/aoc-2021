05bf1c257ffb64731ff800bdaa193cecc45b1855
Samo Prelog <samo@prelog.io>
2021-12-07 05:10:29 +0000

    day 7 - part 2

--- Day07.cs
+++ Day07.cs
@@ -31,4 +31,29 @@ public class Day07
 
         Assert.Equal(expectedOutput, minFuel);
     }
+
+    [Theory]
+    [InlineData("Day07_Example01", 168)]
+    [InlineData("Day07_Input", 99053143)]
+    public void Part02(string input, int expectedOutput)
+    {
+        var data = LoadData(input).Split(',').Select(int.Parse);
+
+        var min = data.Min();
+        var max = data.Max();
+
+        var minFuel = int.MaxValue;
+        var minPos = int.MaxValue;
+        for (var i = min; i <= max; i++)
+        {
+            var fuelCost = data.Sum(x => Enumerable.Range(1, (int)Math.Abs(i - x)).Sum());
+            if (fuelCost < minFuel)
+            {
+                minFuel = fuelCost;
+                minPos = i;
+            }
+        }
+
+        Assert.Equal(expectedOutput, minFuel);
+    }
 }
