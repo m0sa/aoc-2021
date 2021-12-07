110faf75e93cf6f85351c0ab5a19f9617628a40a
Samo Prelog <samo@prelog.io>
2021-12-07 05:23:31 +0000

    day 7 - cleanup

--- Day07.cs
+++ Day07.cs
@@ -4,40 +4,24 @@ namespace aoc_2021;
 
 public class Day07
 {
-    private readonly ITestOutputHelper _helper;
-    public Day07(ITestOutputHelper helper) => _helper = helper;
-
-    [Theory]
-    [InlineData("Day07_Example01", 37)]
-    [InlineData("Day07_Input", 352254)]
-    public void Part01(string input, int expectedOutput)
+    public static int FuelCost1(int position, int target) => (int)Math.Abs(target - position);
+    public static int FuelCost2(int position, int target)
     {
-        var data = LoadData(input).Split(',').Select(int.Parse);
-
-        var min = data.Min();
-        var max = data.Max();
-
-        var minFuel = int.MaxValue;
-        var minPos = int.MaxValue;
-        for (var i = min; i <= max; i++)
-        {
-            var fuelCost = data.Sum(x => (int)Math.Abs(i - x));
-            if (fuelCost < minFuel)
-            {
-                minFuel = fuelCost;
-                minPos = i;
-            }
-        }
-
-        Assert.Equal(expectedOutput, minFuel);
+        // sum of arithmetic sequence
+        var n = (int)Math.Abs(position - target);
+        return n * (1 + n) / 2;
     }
 
     [Theory]
-    [InlineData("Day07_Example01", 168)]
-    [InlineData("Day07_Input", 99053143)]
-    public void Part02(string input, int expectedOutput)
+    [InlineData(nameof(FuelCost1), "Day07_Example01", 37)]
+    [InlineData(nameof(FuelCost1), "Day07_Input", 352254)]
+    [InlineData(nameof(FuelCost2), "Day07_Example01", 168)]
+    [InlineData(nameof(FuelCost2), "Day07_Input", 99053143)]
+    public void Tests(string method, string input, int expectedOutput)
     {
         var data = LoadData(input).Split(',').Select(int.Parse);
+        var testMethod = typeof(Day07).GetMethod(method, BindingFlags.Public | BindingFlags.Static) ?? throw new ArgumentException($"Method {method} not found", nameof(method));
+        var fuelCostMethod = testMethod.CreateDelegate<Func<int, int, int>>();
 
         var min = data.Min();
         var max = data.Max();
@@ -46,7 +30,7 @@ public class Day07
         var minPos = int.MaxValue;
         for (var i = min; i <= max; i++)
         {
-            var fuelCost = data.Sum(x => Enumerable.Range(1, (int)Math.Abs(i - x)).Sum());
+            var fuelCost = data.Sum(x => fuelCostMethod(x, i));
             if (fuelCost < minFuel)
             {
                 minFuel = fuelCost;
