ce045a7d855c5ef5eb60deff5cc975e76c6d4b2d
Samo Prelog <samo@prelog.io>
2021-12-07 05:06:40 +0000

    day 7 - part 1

---
+++ Day07.cs
@@ -0,0 +1,34 @@
+using Xunit.Abstractions;
+
+namespace aoc_2021;
+
+public class Day07
+{
+    private readonly ITestOutputHelper _helper;
+    public Day07(ITestOutputHelper helper) => _helper = helper;
+
+    [Theory]
+    [InlineData("Day07_Example01", 37)]
+    [InlineData("Day07_Input", 352254)]
+    public void Part01(string input, int expectedOutput)
+    {
+        var data = LoadData(input).Split(',').Select(int.Parse);
+
+        var min = data.Min();
+        var max = data.Max();
+
+        var minFuel = int.MaxValue;
+        var minPos = int.MaxValue;
+        for (var i = min; i <= max; i++)
+        {
+            var fuelCost = data.Sum(x => (int)Math.Abs(i - x));
+            if (fuelCost < minFuel)
+            {
+                minFuel = fuelCost;
+                minPos = i;
+            }
+        }
+
+        Assert.Equal(expectedOutput, minFuel);
+    }
+}
